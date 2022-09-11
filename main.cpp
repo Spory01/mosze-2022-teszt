4a1ce0892cd29215cf7724938d3f5bddbf0cc54e
Biró Dániel <birdanae@gmail.com>
2022-09-11 19:54:46 +0200

    hibak javitasa

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];    // nem jo a valtozo neve
-    std::cout << '1-100 ertekek duplazasa'  // nem karaktert irunk ki hanem sztringet, sor vegen nincs pontosvesszo
-    for (int i = 0;)    // nem jol van megadva a for ciklus
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa";
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2;   // ezzel 0-tol kezdodne a szamozas nem 1-tol
+        b[i] = (i + 1) * 2;
     }
-    for (int i = 0; i; i++) // for ciklus rosszul van megirva
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"   // nincs a sor vegen ; , valamint celszeru megjeleniteni magat az erteket is
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;  // nincs megadva kezdoertek
-    for (int i = 0; i < N_ELEMENTS, i++)    // for ciklusban , van ; helyett
+    int atlag = 0;  
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]   // nincs a sor vegen ;
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
